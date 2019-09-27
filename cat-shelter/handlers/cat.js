fe3694ce7acbbb46eb9421f39a6f73778e77d7c8
KaloyanTodorov <kaloyanpetev@gmail.com>
2019-09-27 16:23:33 +0300

    Added Add Cat functionality to Cat handler

--- cat-shelter/handlers/cat.js
+++ cat-shelter/handlers/cat.js
@@ -16,9 +16,13 @@ module.exports = (req, res) => {
         let filePath = path.normalize(path.join(__dirname, '../views/addCat.html'));
 
         const index = fs.createReadStream(filePath);
-    
+        
         index.on('data', (data) => {
-            res.write(data);
+
+            let catBreedPalceholder = breeds.map((breed) => `<option value="${breed}">${breed}</option>`);
+            let modifiedData = data.toString().replace('{{catBreed}}', catBreedPalceholder);
+            
+            res.write(modifiedData);
         })
     
         index.on('end', () => {
@@ -30,7 +34,48 @@ module.exports = (req, res) => {
         })
 
     } else if(pathname === '/cats/add-cat' && req.method === 'POST') {
+        
+        let form = new formidable.IncomingForm();
 
+        form.parse(req, (err, fields, files) => {
+
+            if(err) {
+                throw err;
+            }
+
+            let oldPath = files.upload.path;
+            let newPath = path.normalize(path.join(globalPath, `/content/images/${files.upload.name}`));
+
+            fs.rename(oldPath, newPath, (err) => {
+                if(err) {
+                    console.log("Files were uploaded unsuccsessfully.");
+                    throw err;
+                }
+            });
+
+            fs.readFile('./data/cats.json', 'utf-8', (err, data) => {
+
+                if(err) {
+                    console.log("Uploaded unsuccsessfully23");
+                    throw err;
+                    
+                }
+
+                let allCats = JSON.parse(data);
+                allCats.push({ 
+                    id: cats.length + 1,
+                    ...fields,
+                    image: files.upload.name
+                });
+
+                let json = JSON.stringify(allCats);
+
+                fs.writeFile('./data/cats.json', json, () => {
+                    res.writeHead(102, {location: '/'});
+                    res.end();
+                })                
+            })
+        });
 
     } else if (pathname === '/cats/add-breed' && req.method === 'GET') {
 
